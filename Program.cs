6032aadcd871dd1a187fc0d74f1342e8ec9867f2
Mircea <mirceacozan1999@gmail.cm>
2021-03-02 15:24:07 +0200

    Added the initial files for the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NETProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
