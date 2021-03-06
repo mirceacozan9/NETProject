3f2b1c8989ad5333258aade0301b1d160b6f2a08
Mircea <mirceacozan1999@gmail.cm>
2021-03-06 20:19:10 +0200

    SHA1 now working, will read the file content and compute the MD(no gui)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace NETProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dir = System.IO.Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string filepath = dir + @"\TestSHA1.txt";
+
+            ConvertFileToString converter = new ConvertFileToString(filepath);
+            string text = converter.GetText();
+
+            SHA1Compute hash = new SHA1Compute(text);
+            string final = hash.Hash();
+
+            Console.WriteLine(final);
+            Console.ReadKey();
         }
     }
 }
