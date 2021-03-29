18adaa44ef21bcb430c8cc167e9a4638916b41b6
Mircea <mirceacozan1999@gmail.cm>
2021-03-29 17:05:10 +0300

    SHA1 reworked, AES enc working, no gui

---
+++ SecurityTool/AESCompute.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NETProject
+{
+    class AESCompute
+    {
+        public AESCompute()
+        {
+
+        }
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        public static void FileEncrypt(string inputFile, string password)
+        {
+
+            byte[] salt = GenerateRandomSalt();
+
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.ECB;
+
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                File.Delete(inputFile);
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        public static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.ECB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                File.Delete(inputFile);
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+    }
+}
---
+++ SecurityTool/SHA1Compute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace NETProject
+{
+    public class SHA1Compute
+    {
+
+        public SHA1Compute(string source)
+        {
+            Source = source;
+        }
+
+        public string Source { get; }
+
+        public string Hash()
+        {
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(Source);
+                byte[] hashBytes = sha1Hash.ComputeHash(sourceBytes);
+                string hashFinal = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                return FormatHash(hashFinal);
+            }
+        }
+
+        public string FormatHash(string hash)
+        {
+            for (int i = 4; i <= hash.Length; i += 4)
+            {
+                hash = hash.Insert(i, "-");
+                i++;
+            }
+            hash = hash.Remove(hash.Length - 1);
+            return hash;
+        }
+
+    }
+}
